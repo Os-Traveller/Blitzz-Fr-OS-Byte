13ba11eb26029c19e0720f610800fe923bc9f5d6
Os-Traveller <faisal.ahmeed.20.35.197@gmail.com>
2023-03-01 01:13:05 +0600

    Project Initiated

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    // "./app/**/*.{js,ts,jsx,tsx}",
+    // "./pages/**/*.{js,ts,jsx,tsx}",
+    // "./components/**/*.{js,ts,jsx,tsx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: "class",
+  // theme: {
+  //   extend: {
+  //     colors: {
+  //       primary: {
+  //         100: "#d7e5ff",
+  //         200: "#afcbff",
+  //         300: "#87b1ff",
+  //         400: "#5f97ff",
+  //         500: "#377dff",
+  //         600: "#2c64cc",
+  //         700: "#214b99",
+  //         800: "#163266",
+  //         900: "#0b1933",
+  //       },
+  //       main: "#3b404b",
+  //       whiteGray: "#F9FAFB",
+  //       black: "#18191A",
+  //       blackGray: "#242526",
+  //     },
+  //   },
+  // },
+  plugins: [],
+};
